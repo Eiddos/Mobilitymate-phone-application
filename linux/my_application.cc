81161db9cfbac52babfc99ed04694e6021c06926
ashrafAbdoMohamed <47904150+ashrafAbdoMohamed@users.noreply.github.com>
2023-12-08 13:41:33 +0200

    Add files via upload

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "rondom_number");
+    gtk_header_bar_set_title(header_bar, "mobilitymate");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "rondom_number");
+    gtk_window_set_title(window, "mobilitymate");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
